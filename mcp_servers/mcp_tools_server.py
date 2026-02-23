28595a8c3ea840a0fb0bc1da5a90554e4182ca34
Sorin Manole <somanole@gmail.com>
2026-02-23 15:06:05 +0100

    implemented mcp server with various tool

--- mcp_servers/mcp_tools_server.py
+++ mcp_servers/mcp_tools_server.py
@@ -1,14 +1,30 @@
 import os
+import sys
 from mcp.server.fastmcp import FastMCP
 from duckduckgo_search import DDGS
 
 
+############## For NOTEBOOK 3_MCP_with_memory.ipynb ##############
+## Uncomment the following lines to connect to Redis for the notebook exercises.
+
+# from mcp.server.transport_security import TransportSecuritySettings
+# mcp: FastMCP = FastMCP("MemoryServer", 
+#                        host="127.0.0.1", 
+#                        port=8000,
+#                        transport_security=TransportSecuritySettings(enable_dns_rebinding_protection=False)
+#                        )
+
+
+############### For NOTEBOOK 3_MCP_with_memory.ipynb ##############
+## Comment this line out if you want to connect to Redis.
 mcp: FastMCP = FastMCP("SuperServer")
+
+
 
 @mcp.tool()
 def search_web(query: str) -> str:
     """Searches the web for current information using DuckDuckGo."""
-    print(f"DEBUG: Searching web for {query}") # FastMCP redirects prints safely
+    sys.stderr.write(f"DEBUG: Searching web for {query}\n") # FastMCP redirects prints safely
     with DDGS() as ddgs:
         results = [r['body'] for r in ddgs.text(query, max_results=3)]
         return "\n---\n".join(results) if results else "No results found."
@@ -27,4 +43,9 @@ def read_local_file(file_path: str) -> str:
 
     
 if __name__ == "__main__":
-    mcp.run()
+    # mcp.run()
+    
+    ## For NOTEBOOK 3_MCP_with_memory.ipynb, use this line instead to run with SSE transport:
+    # comment the above line
+    
+    mcp.run(transport="sse")
